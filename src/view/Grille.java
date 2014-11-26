90df8a1a68fd054d7f293456ad58248ad2364fdf
mathieuberthome <mathieuberthome22@gmail.com>
2014-11-26 21:29:07 +0100

    Update Grille.java

--- src/view/Grille.java
+++ src/view/Grille.java
@@ -5,7 +5,7 @@ import java.awt.*;
 
 public class Grille extends JFrame {
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Champ de bataille");
+		JFrame frame = new JFrame("Bataille Navale");
 		frame.setSize(350,350);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
